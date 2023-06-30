4e25f70b3ff2c42b840e73d78a6358d2f3e5c51c
jetblack <coolanimeguy666@gmail.com>
2023-06-29 19:27:57 -0700

    Added some stuff

--- rpg-test/InputHandler.js
+++ rpg-test/InputHandler.js
@@ -5,25 +5,38 @@ import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.j
 // const {camera} = require('./main.js');
 // const {window} = require('./main.js');
 import { camera } from './main.js'
-
+var direction = [false, false, false ,false]
+export {direction};
 
 window.addEventListener('keydown', function(event){
-    console.log(event.key, " is key");
+    // console.log(event.key, " is key");
+
+    //theta = Math.atan2(vector.x,vector.z);
+    console.log("The rotation is ", camera.rotation.y);
+    //camera.rotation.y = what you are looking at 
+    
+
+    if (radians == Math.PI/2){
+        console.log("you are looking left");
+    }
+        // console.log(radians, "is radians");
+
     switch(event.key){
-      case 'w':
-        camera.y += 0.1;
-        break;
-      case 's':
-        break;
-      case 'd':
-        break;
-      case 'a':
-        break;
-      case 'q' : 
-        camera.rotation.y -=0.1;
-        break;
-      case 'e':
-        camera.rotation.y +=0.1;
-        break;
+        case 'w':
+
+            camera.y += 0.1;
+            break;  
+        case 's':
+            break;
+        case 'd':
+            break;
+        case 'a':
+            break;
+        default:
+            var heading = camera.rotation.y;
+            var radians = heading > 0 ? heading : (2 * Math.PI) + heading;
+            radians = radians % (Math.PI *2);
+            camera.rotation.y +=Math.PI/2;
+
     }
   })
